21cc28307ff6013e7e3b0675f294c2f9014ec612
daadestroyer <nigamshubham2000@gmail.com>
2021-06-24 00:50:34 +0530

    implement queue using linkedlist

--- CB-Crux/src/queues/App02_QueueUsingLinkedList.java
+++ CB-Crux/src/queues/App02_QueueUsingLinkedList.java
@@ -4,7 +4,42 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class App02_QueueUsingLinkedList {
+
+	LinkedList<Integer> queue;
+
+	public App02_QueueUsingLinkedList() {
+		queue = new LinkedList<Integer>();
+	}
+
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+
+	public void enqueue(int data) {
+		queue.addLast(data);
+	}
+
+	public void dequeue() {
+		queue.removeFirst();
+	}
+
+	public int getFront() {
+		return queue.getFirst();
+	}
+
 	public static void main(String[] args) {
-		Queue<Integer> q = new LinkedList<Integer>();
+		App02_QueueUsingLinkedList queueLL = new App02_QueueUsingLinkedList();
+		for (int i = 1; i <= 5; i++) {
+			queueLL.enqueue(i);
+		}
+		
+		queueLL.dequeue();
+		queueLL.dequeue();
+		queueLL.enqueue(6);
+		
+		while(!queueLL.isEmpty()) {
+			System.out.println(queueLL.getFront());
+			queueLL.dequeue();
+		}
 	}
 }
