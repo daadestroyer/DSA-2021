cb0a1d977800a9bfe85181c4a9ae25ad89b68877
daadestroyer <nigamshubham2000@gmail.com>
2021-07-04 11:00:18 +0530

    binary tree postOrder done....

--- CB-Crux/src/trees/App02_BinaryTree.java
+++ CB-Crux/src/trees/App02_BinaryTree.java
@@ -72,11 +72,9 @@ public class App02_BinaryTree {
 		} else {
 			string = string + "END<=";
 		}
-		
+
 		string = string + node.data + "=>";
-		
-		
-		
+
 		if (node.rightNode != null) {
 			string = string + node.rightNode.data;
 		} else {
@@ -96,11 +94,69 @@ public class App02_BinaryTree {
 		this.display(this.rootNode);
 	}
 
+	private int height(Node rootNode) {
+		if (rootNode == null) {
+			return -1;
+		}
+
+		// calculate left subtree height
+		int lheight = height(rootNode.leftNode);
+
+		// calculate right subtree height
+		int rheight = height(rootNode.rightNode);
+
+		int max = Math.max(lheight, rheight);
+
+		return max + 1;
+
+	}
+
+	public int height() {
+		return this.height(this.rootNode);
+	}
+
+	private void postOrder(Node rootNode) {
+		if (rootNode == null) {
+			return;
+		}
+		postOrder(rootNode.leftNode);
+		postOrder(rootNode.rightNode);
+		System.out.print(rootNode.data + " ");
+
+	}
+
+	public void postOrder() {
+		this.postOrder(this.rootNode);
+		System.out.print("END");
+	}
+	
+	private void preOrder(Node rootNode) {
+		if (rootNode == null) {
+			return;
+		}
+		System.out.print(rootNode.data + " ");
+		postOrder(rootNode.leftNode);
+		postOrder(rootNode.rightNode);
+		
+
+	}
+
+	public void preOrder() {
+		this.preOrder(this.rootNode);
+		System.out.print("END");
+	}
+
 	public static void main(String[] args) {
 		App02_BinaryTree binaryTree = new App02_BinaryTree();
 		System.out.println();
 		binaryTree.display();
 
-		// 50 true 25 true 38 false false true 48 true 18 false false false true 45 true 85 false false true 60 false false
+// 50 true 25 true 38 false false true 48 true 18 false false false true 45 true 85 false false true 60 false false
+
+		System.out.println();
+		System.out.println("height = " + binaryTree.height());
+		binaryTree.postOrder();
+		binaryTree.preOrder();
+
 	}
 }
