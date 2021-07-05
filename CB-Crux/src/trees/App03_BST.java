57c879b5264c5635d866c84b37c858697ec6b93a
daadestroyer <nigamshubham2000@gmail.com>
2021-07-05 20:37:36 +0530

    BST Creation done...

---
+++ CB-Crux/src/trees/App03_BST.java
@@ -0,0 +1,46 @@
+package trees;
+
+/*
+
+*/
+public class App03_BST {
+
+	private class Node {
+
+		int data;
+		Node leftNode;
+		Node rightNode;
+
+	}
+	private Node rootNode;
+	
+	public App03_BST(int[] ar) {
+		this.rootNode = construct(ar,0,ar.length-1);
+	}
+	
+	private Node construct(int[] ar , int low , int hi) {
+		
+		if(low>hi){
+			return null;
+		}
+		// find mid
+		int mid = (low+hi)/2;
+		
+		// create a new node
+		Node node = new Node();
+		node.data = ar[mid];
+		
+		// left recursive call from low to mid-1;
+		
+		node.leftNode = construct(ar, low, mid-1);
+		node.rightNode = construct(ar, mid+1, hi);
+		
+		return node;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		App03_BST bst = new App03_BST(new int[] {10,20,30,40,50,60,70});
+	}
+}
